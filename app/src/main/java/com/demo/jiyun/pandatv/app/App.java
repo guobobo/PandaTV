434ad0bd36be79c71e7152134a8eca6c0f50ac99
gulajiasiqi <1211039589@qq.com>
2017-07-31 21:30:23 +0800

    _7.31消息推送

--- app/src/main/java/com/demo/jiyun/pandatv/app/App.java
+++ app/src/main/java/com/demo/jiyun/pandatv/app/App.java
@@ -4,6 +4,8 @@ package com.demo.jiyun.pandatv.app;
 import android.app.Application;
 
 import com.demo.jiyun.pandatv.base.BaseActivity;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
 import com.umeng.socialize.Config;
 import com.umeng.socialize.PlatformConfig;
 import com.umeng.socialize.UMShareAPI;
@@ -23,6 +25,24 @@ public class App extends Application {
         super.onCreate();
         Config.DEBUG=true;
         UMShareAPI.get(this);
+
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+//注册推送服务，每次调用register方法都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+
+            @Override
+            public void onSuccess(String deviceToken) {
+                //注册成功会返回device token
+//                Log.e("TAG","deviceToken:"+deviceToken);
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+
+            }
+        });
+
     }
+
 
 }
